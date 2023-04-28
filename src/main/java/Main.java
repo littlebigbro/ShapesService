2ea52887e6db1e7c144d8a5193103067b854e080
ilya hw-notebook <itsimkovskiy@gmail.com>
2023-04-28 10:57:13 +0300

    change of structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,16 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 По Вашему усмотрению программа может быть с выводом графическим ( консоль, графическое приложение, веб-проект), либо без (таким образом на вход файл - результат тоже файл)
 
 **/
+
+/*
+* 0) Поправить структуру проекта
+* 1) Перейти на реляционную бд
+* 2) подключить spring jpa
+* 3) использовать dto
+* 4) добавить тесты и jacoco
+* 5) добавить checkstyle
+*
+* */
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Main {
