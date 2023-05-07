0ee6bba38a660bfe954ecc198f6b1daace16f986
ilya hw-notebook <itsimkovskiy@gmail.com>
2023-05-07 15:57:01 +0300

    Add validation for DTO's

---
+++ src/main/java/shapes/models/dto/fieldNames/ShapeFieldNames.java
@@ -0,0 +1,10 @@
+package shapes.models.dto.fieldNames;
+
+public interface ShapeFieldNames {
+    String SHAPE_ID = "id фигуры в базе данных";
+    String CREATED = "дата создания фигуры";
+    String UPDATED = "дата обновления фигуры";
+    String SHAPE_TYPE = "тип фигуры";
+    String POINTS = "точки фигуры";
+    String RADIUS_INFO = "информация о радиусе фигуры";
+}
